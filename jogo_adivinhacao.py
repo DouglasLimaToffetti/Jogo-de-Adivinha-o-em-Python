c4d5e0bab62f54b20e96fdb833300e24b00f76a5
Doug <douglas.lima@igtcoaching.com>
2024-10-24 00:30:31 -0300

    Jogo de adivinhação em Python

---
+++ jogo_adivinhacao.py
@@ -0,0 +1,30 @@
+import random
+
+def jogar_adivinhacao():
+    numero_adivinhar = random.randint(1, 100)
+    tentativas = 0
+
+    print('Seja bem-vindo ao jogo da adivinhação!!')
+    print('Tente adivinhar o número que estou pensando entre 1 e 100.')
+
+    while True:
+        palpite = int(input('Digite seu palpite: '))
+        tentativas += 1
+
+        if palpite < numero_adivinhar:
+            print('Tente um número maior.')
+        elif palpite > numero_adivinhar:
+            print('Tente um número menor.')
+        else:
+            print(f'Boaaaaa, você acertou o número {numero_adivinhar} em {tentativas} tentativas')
+            break
+        
+def main():
+    while True:
+        jogar_adivinhacao()
+        jogar_novamente = input('Deseja jogar o jogo novamente? (s/n): ').lower()
+
+        if jogar_novamente != 's':
+            print('Obrigado por jogar :)')
+            break
+main()
